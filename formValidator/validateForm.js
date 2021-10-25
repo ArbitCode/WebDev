010d078015a320a7bbb3db97cd56197f28cc8c67
ArbitCode <rajaramsharma422@gmail.com>
2021-10-26 01:42:02 +0530

    Adding login UI design

--- formValidator/validateForm.js
+++ formValidator/validateForm.js
@@ -3,7 +3,7 @@ const form= document.getElementById('form');
 const username = document.getElementById('username');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
-const cPassword = document.getElementById('c-password');
+const cPassword = document.getElementById('re-TypePassword');
 
 //show error mesage
 
